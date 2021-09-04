51964f2c7d4aa847566258950ba5f42247e880c3
Tracey <ttrepper@yahoo.com>
2021-09-04 18:30:21 -0400

    deleted venv

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,35 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+app = Flask(__name__)
+
+@app.route('/add')
+def add_it():
+    """Add a and b."""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a,b)
+    return str(result)
+
+@app.route('/sub')
+def sub_it():
+    """Substract b from a."""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a,b)
+    return str(result)
+
+@app.route('/mult')
+def mult_it(a, b):
+    """Multiply a and b."""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = mult(a,b)
+    return str(result)
+
+@app.route('/div')
+def div_it(a, b):
+    """Divide a by b."""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = div(a,b)
+    return str(result)
---
+++ greet/app.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+app = Flask(__name__)
+@app.route('/welcome')
+def welcome():
+    return "Welcome!"
+@app.route('/welcome/home')
+def welcome_home():
+    return "Welcome Home!"
+@app.route('/welcome/back')
+def welcome_back():
+    return "Welcome Back!"
